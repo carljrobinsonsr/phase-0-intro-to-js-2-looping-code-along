fa5469a6d80e221855a7716d152ed23a55789d27
carljrobinsonsr <carljrobinsonsr@gmail.com>
2021-03-23 19:44:31 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solutions in this file
+{function writeCards (names,event) 
+{const messages = []
+   
+   
+   
+    for (let i = 0; i < names.length; i++)
+     {'Thank you, ${names[i]} for the wonderful ${event} gift';}
+     messages.push(message);
+    return messages }
+}
